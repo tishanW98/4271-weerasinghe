8fd707e653c082ae49951f515ae68b1a098cf936
tishanW98 <shakilatishan1998@outlook.com>
2024-04-22 20:13:14 +0530

    file-removed

--- nodeapp/test/test.js
+++ nodeapp/test/test.js
@@ -1,7 +1,7 @@
-var request = require('supertest');
-var app = require('../index.js');
-describe('GET /will', function() {
-    it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
-    });
+var request = require("supertest");
+var app = require("../index.js");
+describe("GET /status", function () {
+  it("respond with hello world", function (done) {
+    request(app).get("/status").expect('{ "response": "Hello World" }', done);
+  });
 });
